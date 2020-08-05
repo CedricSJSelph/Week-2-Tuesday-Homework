6c93ee10346bd57d922101ed768503436e27ea2a
koastiebratt@gmail.com <you@example.com>
2020-08-05 08:24:00 -0400

    Changed Variable Data Type

--- src/week_2_Homework/CelciusToFarenheit.java
+++ src/week_2_Homework/CelciusToFarenheit.java
@@ -6,7 +6,7 @@ public class CelciusToFarenheit {
 	public static void main(String[] args) {
 		
 		double Converted_Temp;
-		int Celcius;
+		double Celcius;
 		
 		Scanner scan = new Scanner(System.in);
 		
